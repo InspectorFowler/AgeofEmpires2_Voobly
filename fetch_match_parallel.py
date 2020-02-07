21ccc4c3bae7ddb4f73464d6e35da5bd797f769e
Parikshit Verma <verma.parikshit@gmail.com>
2020-02-07 01:12:18 -0600

    incremental update

---
+++ fetch_match_parallel.py
@@ -0,0 +1,94 @@
+import sys
+import pandas as pd
+from functools import partial
+
+import time
+
+import mechanize
+import http.cookiejar
+import requests
+
+import multiprocessing as mp
+from multiprocessing import Pool
+from ipynb.fs.full.voobly_scraping_modules import fetch_all_match_details
+from ipynb.fs.full.voobly_scraping_modules import setup_sql_conn
+from ipynb.fs.full.credentials import credentials
+
+start_id = int(sys.argv[1])
+collect = int(sys.argv[2])
+procs = int(sys.argv[3])
+
+def fetch_matches(match):
+    
+    # Create connection instance
+    username, password = credentials()
+    instance = voobly_login(username,password)
+    
+     # Set up SQL connection
+    db_conn,db_cursor,engine = setup_sql_conn()   
+    
+    obj = pd.Series.to_frame(pd.Series([match], dtype='str'))
+    obj.columns = ['Match_ID']       
+
+    try:            
+        data = fetch_all_match_details(match,instance)
+
+        if len(data) == 1 and data == [2]:
+            obj.to_sql('NONEXISTENT_FAILS_TEMP',con=engine, if_exists="append",index=False)
+
+        elif len(data) == 1 and data == [1]:
+            obj.to_sql('TIMEOUT_FAILS_TEMP',con=engine, if_exists="append",index=False)
+
+        elif len(data) == 1 and data == [3]:
+            obj.to_sql('NONAOE2MATCH_FAILS_TEMP',con=engine, if_exists="append",index=False)
+            
+        else:
+            data.to_sql('RAW_MATCH_DATA_TEMP',con=engine, if_exists="append",index=False)
+    except:
+        obj.to_sql('UNKNOWN_FAILS_TEMP',con=engine, if_exists="append",index=False)
+        
+def voobly_login(username, password):
+    # Browser
+    br = mechanize.Browser()
+
+    # Cookie Jar
+    cj = http.cookiejar.LWPCookieJar()
+    br.set_cookiejar(cj)
+
+    # Browser options
+    br.set_handle_equiv(True)
+    br.set_handle_gzip(True)
+    br.set_handle_redirect(True)
+    br.set_handle_referer(True)
+    br.set_handle_robots(False)
+    br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+
+    br.addheaders = [('User-agent', 'Chrome')]
+
+    # The site we will navigate into, handling it's session
+    br.open('https://voobly.com/login')
+
+    # Select the second (index one) form (the first form is a search query box)
+    br.select_form(nr=0)
+
+    # User credentials
+    br.form['username'] = username
+    br.form['password'] = password
+
+    # Login
+    br.submit()
+    
+    return(br)        
+        
+if __name__ == '__main__':       
+    
+    start = time.time()
+    iterations = list(range(start_id,start_id+collect))
+    
+    # Parallel processing
+    with mp.Pool(processes=procs) as p:
+        p.map(fetch_matches, iterations)
+        
+    print("Time taken to scrap "+str(collect)+" matches: "+str(time.time() - start))
+
+    
