ba4f534f621d3938432b2cdfeca2da1b4d28b395
Parikshit Verma <verma.parikshit@gmail.com>
2020-03-15 23:36:48 -0500

    update

---
+++ setup_sql_conn.py
@@ -0,0 +1,24 @@
+import pyodbc
+from sqlalchemy import create_engine
+
+def setup_sql_conn(server, database):
+    
+    driver = '{SQL Server Native Client 11.0}'
+    port = 1433
+
+    db_conn = pyodbc.connect('DRIVER='+driver+
+                             ';PORT='+str(port)+
+                             ';SERVER='+server+
+                             ';DATABASE='+database+
+                             ';Trusted_Connection=yes')
+
+    db_cursor = db_conn.cursor()
+
+    engine = create_engine("mssql+pyodbc://"+server+"/"+database+"?driver=SQL+Server+Native+Client+11.0")
+    return(db_conn,db_cursor,engine)
+
+# fetch data from DB
+def fetch_sql_data(query,db_conn):
+    
+    data = pd.DataFrame(pd.read_sql_query(query, db_conn))
+    return(data)
