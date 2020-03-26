4d5a903f16c64894fff6b615cb88ae529d8d387c
Parikshit Verma <verma.parikshit@gmail.com>
2020-03-25 23:23:19 -0500

    update

---
+++ Input/match_data_query.sql
@@ -0,0 +1,14 @@
+SELECT *
+FROM [AOE2_VOOBLY].[dbo].[MATCH_DATA]
+WHERE ladder = 'Ladder: RM - 1v1' AND
+	  Match_map IN (SELECT Match_map
+					FROM [AOE2_VOOBLY].[dbo].[MATCH_DATA]
+					WHERE ladder = 'Ladder: RM - 1v1'
+					GROUP BY Match_map
+					HAVING COUNT(*) > 100) AND
+	  Match_mod IN (SELECT Match_mod
+					FROM [AOE2_VOOBLY].[dbo].[MATCH_DATA]
+					WHERE ladder = 'Ladder: RM - 1v1'
+					GROUP BY Match_mod
+					HAVING COUNT(*) > 1000) AND
+	  Match_length > 5 
