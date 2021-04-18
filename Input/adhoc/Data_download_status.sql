aae5487208cd894dee5edb227c11d1edb172bdde
Parikshit Verma <verma.parikshit@gmail.com>
2021-04-18 15:21:13 -0500

    bulk update

--- Input/adhoc/Data_download_status.sql
+++ Input/adhoc/Data_download_status.sql
@@ -1,35 +1,17 @@
-/****** Script for SelectTopNRows command from SSMS  ******/
+select 'Total' as ladder, 
+	   count(*) as row_cnt,
+	   count(distinct Match_ID) as match_cnt,
+	   max(Match_date) as max_date,
+	   min(Match_date) as min_date
+from RAW_MATCH_DATA rmd
 
-SELECT TOP 100 * FROM [AOE2_VOOBLY].[dbo].[RAW_MATCH_DATA] A
+union
 
-SELECT 'Total' AS ladder,
-	   COUNT(DISTINCT Match_ID) AS MATCH_CNT,
-	   COUNT(*) AS ROW_CNT,
-	   MAX(CAST(Match_ID AS INT)) AS MAX_MATCH_ID,
-	   MIN(convert_match_date) AS MIN_DATE,
-	   MAX(convert_match_date) AS MAX_DATE
-FROM (SELECT A.*,
-			 CASE WHEN CHARINDEX('/',Match_date) <> 0 
-				THEN CAST((CAST(CAST(LEFT(Match_date,(CHARINDEX('/',Match_date)-1)) AS INT) AS VARCHAR) +
-						'-' + CAST(CAST(REPLACE(SUBSTRING(Match_date,CHARINDEX('/',Match_date)+1,LEN(Match_date)-7),'/','') AS INT) AS VARCHAR) +
-						'-' + RIGHT(Match_date,4)) AS DATE) 
-				ELSE NULL END AS convert_match_date
-	  FROM [AOE2_VOOBLY].[dbo].[RAW_MATCH_DATA] A) M
-
-UNION
-
-SELECT ladder,
-	   COUNT(DISTINCT Match_ID) AS MATCH_CNT,
-	   COUNT(*) AS ROW_CNT,
-	   MAX(CAST(Match_ID AS INT)) AS MAX_MATCH_ID,
-	   MIN(convert_match_date) AS MIN_DATE,
-	   MAX(convert_match_date) AS MAX_DATE
-FROM (SELECT A.*,
-			 CASE WHEN CHARINDEX('/',Match_date) <> 0 
-				THEN CAST((CAST(CAST(LEFT(Match_date,(CHARINDEX('/',Match_date)-1)) AS INT) AS VARCHAR) +
-						'-' + CAST(CAST(REPLACE(SUBSTRING(Match_date,CHARINDEX('/',Match_date)+1,LEN(Match_date)-7),'/','') AS INT) AS VARCHAR) +
-						'-' + RIGHT(Match_date,4)) AS DATE) 
-				ELSE NULL END AS convert_match_date
-	  FROM [AOE2_VOOBLY].[dbo].[RAW_MATCH_DATA] A) M
-GROUP BY ladder
-ORDER BY 2 DESC
+select ladder, 
+	   count(*) as row_cnt,
+	   count(distinct Match_ID) as match_cnt,
+	   max(Match_date) as max_date,
+	   min(Match_date) as min_date
+from RAW_MATCH_DATA rmd
+group by ladder
+order by 3 desc
