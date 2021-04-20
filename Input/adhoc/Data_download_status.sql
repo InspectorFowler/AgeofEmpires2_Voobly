e5aa4c9f85d5e09a445f8e077e3aacdd0020a095
Parikshit Verma <verma.parikshit@gmail.com>
2021-04-20 02:04:53 -0500

    update sql queries

--- Input/adhoc/Data_download_status.sql
+++ Input/adhoc/Data_download_status.sql
@@ -1,8 +1,8 @@
 select 'Total' as ladder, 
 	   count(*) as row_cnt,
 	   count(distinct Match_ID) as match_cnt,
-	   max(Match_date) as max_date,
-	   min(Match_date) as min_date
+	   max(STR_TO_DATE(Match_date,'%m/%d/%Y')) as max_date,
+	   min(STR_TO_DATE(Match_date,'%m/%d/%Y')) as min_date
 from RAW_MATCH_DATA rmd
 
 union
@@ -10,8 +10,9 @@ union
 select ladder, 
 	   count(*) as row_cnt,
 	   count(distinct Match_ID) as match_cnt,
-	   max(Match_date) as max_date,
-	   min(Match_date) as min_date
+	   max(STR_TO_DATE(Match_date,'%m/%d/%Y')) as max_date,
+	   min(STR_TO_DATE(Match_date,'%m/%d/%Y')) as min_date
 from RAW_MATCH_DATA rmd
 group by ladder
 order by 3 desc
+
